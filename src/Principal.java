0ee4cf247ce2de62f6b800e034e5c6d8a3c16906
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-09 09:45:57 -0300

    criação de objeto

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//tipo nome;
+		int idade ;
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Vinicius";
+	    p1.sexo = "Masc";
+	    p1.idade = 17;
+		System.out.println("Pessoa: " + p1.nome + " tem " +  p1.idade);
 	}
 
 }
